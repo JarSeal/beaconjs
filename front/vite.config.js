1f590e7686ae33247d24d54a7b6a71db001c06a2
JarSeal <kai.forsman@gmail.com>
2022-08-08 23:39:44 +0200

    Front end build logic

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,13 +1,40 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite';
-import 'dotenv/config';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import dotenv from 'dotenv';
+import { normalizePath } from 'vite';
+import path from 'node:path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const _dirname =
+  typeof __dirname !== 'undefined' ? __dirname : dirname(fileURLToPath(import.meta.url));
+
+const ENV = dotenv.config({ path: normalizePath(path.resolve(_dirname, 'src/.env')) }).parsed;
 
 export default defineConfig({
   root: './src',
   build: {
     // Relative to the root
-    outDir: './dist',
+    outDir: '../dist',
+    emptyOutDir: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: normalizePath(path.resolve(_dirname, `dist/index.html`)),
+          dest: normalizePath(path.resolve(_dirname, `../back/front${ENV.VITE_CLIENT_PATH}`)),
+        },
+        {
+          src: normalizePath(path.resolve(_dirname, `dist/assets`)),
+          dest: normalizePath(path.resolve(_dirname, `../back/front`)),
+        },
+      ],
+      flatten: true,
+    }),
+  ],
 });
