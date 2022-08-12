81ef4ea6654ced934845546e77a9a4c7307e0532
JarSeal <kai.forsman@gmail.com>
2022-08-12 12:10:23 +0200

    TEST AGAIN

--- front/vite.config.js
+++ front/vite.config.js
@@ -10,13 +10,19 @@ const _dirname =
   typeof __dirname !== 'undefined' ? __dirname : dirname(fileURLToPath(import.meta.url));
 
 const ENV = dotenv.config({ path: normalizePath(path.resolve(_dirname, 'src/.env')) }).parsed;
+let CLIENT_PATH;
+if (!ENV) {
+  CLIENT_PATH = process.env.VITE_CLIENT_PATH;
+} else {
+  CLIENT_PATH = ENV.VITE_CLIENT_PATH;
+}
 
 export default defineConfig({
   root: './src',
   base: './',
   build: {
     // Relative to the root (where this file is)
-    outDir: `../../back/front${ENV.VITE_CLIENT_PATH}`,
+    outDir: `../../back/front${CLIENT_PATH}`,
     emptyOutDir: true,
     commonjsOptions: {
       transformMixedEsModules: true,
